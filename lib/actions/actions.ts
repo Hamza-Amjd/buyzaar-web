2de122af81dc915295dc9078585e6d27c74868cc
Hamza <135211820+Hamza-Amjd@users.noreply.github.com>
2024-08-24 01:01:41 +0500

    initial commit

---
+++ lib/actions/actions.ts
@@ -0,0 +1,36 @@
+import axios from "axios"
+
+export const getCollections = async () => {
+  const collections = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/collections`).then((response) =>{return response.data}).catch((error) =>(console.error(error)));
+  return collections
+}
+
+export const getCollectionDetails = async (collectionId: string) => {
+  const collection = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/collections/${collectionId}`)
+  return await collection.json()
+}
+
+export const getProducts = async () => {
+  const products = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/products`).then((response) =>{return response.data}).catch((error) =>(console.error(error)));
+  return products
+}
+
+export const getProductDetails = async (productId: string) => {
+  const product = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`)
+  return await product.json()
+}
+
+export const getSearchedProducts = async (query: string) => {
+  const searchedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/search/${query}`)
+  return await searchedProducts.json()
+}
+
+export const getOrders = async (customerId: string) => {
+  const orders = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/orders/customers/${customerId}`)
+  return await orders.json()
+}
+
+export const getRelatedProducts = async (productId: string) => {
+  const relatedProducts = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/related`)
+  return await relatedProducts.json()
+}
